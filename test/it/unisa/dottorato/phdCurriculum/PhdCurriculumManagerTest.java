a9f72cf9d5ac16139e4814f95967b2745353f3f2
Fudo <Fudo@192.168.0.103>
2014-12-10 18:00:46 +0100

    Junit Test Gestione Cicli e Curriculum

---
+++ test/it/unisa/dottorato/phdCurriculum/PhdCurriculumManagerTest.java
@@ -0,0 +1,267 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.dottorato.phdCurriculum;
+
+import it.unisa.dottorato.exception.ConnectionException;
+import it.unisa.dottorato.exception.EntityNotFoundException;
+import it.unisa.dottorato.phdCycle.PhdCycle;
+import it.unisa.dottorato.phdCycle.PhdCycleManager;
+import it.unisa.dottorato.phdCycle.PhdCycleManagerTest;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Fudo
+ */
+public class PhdCurriculumManagerTest {
+    
+    public PhdCurriculumManagerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+        
+        try {
+            PhdCycleManager.getInstance().delete("11");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(PhdCycleManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(PhdCycleManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(PhdCycleManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (EntityNotFoundException ex) {
+            Logger.getLogger(PhdCycleManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ConnectionException ex) {
+            Logger.getLogger(PhdCycleManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Test of getInstance method, of class PhdCycleManager.
+     */
+    @Test
+    public void testGetInstance() {
+        System.out.println("getInstance");
+        PhdCurriculumManager result = PhdCurriculumManager.getInstance();
+        assertNotNull(result);
+    }
+
+    /**
+     * Test of insert method, of class PhdCycleManager.
+     */
+    @Test
+    public void testInsert() throws Exception {
+        System.out.println("insert");
+        PhdCurriculum pCurriculum = new PhdCurriculum();
+        PhdCurriculumManager instance = PhdCurriculumManager.getInstance();
+        pCurriculum.setName("Informatica");
+        pCurriculum.setDescription("descrizione");
+        pCurriculum.setFK_Professor("ertghyuijhtredfg");
+        instance.insert(pCurriculum);
+        
+        PhdCurriculum result = instance.getPhdCurriculumById("Informatica");
+        assertNotNull(result);
+        
+        tearDown();
+    }
+    
+    
+    @Test
+    public void testInsertMinNameCurriculum() throws Exception {
+        System.out.println("insert");
+        PhdCurriculum pCurriculum = new PhdCurriculum();
+        PhdCurriculumManager instance = PhdCurriculumManager.getInstance();
+        pCurriculum.setName("info");
+        pCurriculum.setDescription("descrizione");
+        pCurriculum.setFK_Professor("ertghyuijhtredfg");
+        instance.insert(pCurriculum);
+        
+        PhdCurriculum result = instance.getPhdCurriculumById("Informatica");
+        assertNotNull(result);
+        
+        tearDown();
+    }
+    
+    @Test
+    public void testInsertMaxNameCurriculum() throws Exception {
+        System.out.println("insert");
+        PhdCurriculum pCurriculum = new PhdCurriculum();
+        PhdCurriculumManager instance = PhdCurriculumManager.getInstance();
+        pCurriculum.setName("DGASHDGHASGDJHASGDHJGASHJDHJASGDGASJHGDJHASGJHDGASJHGDHJASGDJHASGJHDGASHJGDHASGJHDGASJHDGJHASGDJHASGJHDGJASHGDHJASGHJDGASHJGDJHSAGFDSHASHJDGASJHDGAHJSGDHJASGDJHASGJHDGASJHDGASHJGDHJASGDJHASGJDHASGJHDGASHJDGASHJDGHJASGDJHASGDJHAGHDJASGJJHDGASJHGD");
+        pCurriculum.setDescription("descrizione");
+        pCurriculum.setFK_Professor("ertghyuijhtredfg");
+        instance.insert(pCurriculum);
+        
+        PhdCurriculum result = instance.getPhdCurriculumById("Informatica");
+        assertNotNull(result);
+        
+        tearDown();
+    }
+    
+    @Test
+    public void testInsertFormatNameCurriculum() throws Exception {
+        System.out.println("insert");
+        PhdCurriculum pCurriculum = new PhdCurriculum();
+        PhdCurriculumManager instance = PhdCurriculumManager.getInstance();
+        pCurriculum.setName("&&&&&&&&&&&&&&");
+        pCurriculum.setDescription("descrizione");
+        pCurriculum.setFK_Professor("ertghyuijhtredfg");
+        instance.insert(pCurriculum);
+        
+        PhdCurriculum result = instance.getPhdCurriculumById("Informatica");
+        assertNotNull(result);
+        
+        tearDown();
+    }
+    
+    
+
+    /**
+     * Test of update method, of class PhdCycleManager.
+     */
+    @Test
+    public void testUpdate() throws Exception {
+        System.out.println("update");
+        PhdCurriculumManager instance = PhdCurriculumManager.getInstance();
+        PhdCurriculum pCurriculum = new PhdCurriculum();
+        String oldCurriculum = pCurriculum.getDescription();
+        pCurriculum.setDescription("UpdateDescrizione");
+        pCurriculum.setName("UpdateInformatica");
+        pCurriculum.setFK_Professor("ertghyuijhtredfg");
+        
+        instance.update(oldCurriculum, pCurriculum);
+        assertEquals(oldCurriculum, pCurriculum);
+        tearDown();
+    }
+    
+    @Test
+    public void testMinNameUpdate() throws Exception {
+        System.out.println("update");
+        PhdCurriculumManager instance = PhdCurriculumManager.getInstance();
+        PhdCurriculum pCurriculum = new PhdCurriculum();
+        String oldCurriculum = pCurriculum.getDescription();
+        pCurriculum.setDescription("UpdateDescrizione");
+        pCurriculum.setName("in");
+        pCurriculum.setFK_Professor("ertghyuijhtredfg");
+        
+        instance.update(oldCurriculum, pCurriculum);
+        assertEquals(oldCurriculum, pCurriculum);
+        tearDown();
+    }
+    
+    @Test
+    public void testMaxNameUpdate() throws Exception {
+        System.out.println("update");
+        PhdCurriculumManager instance = PhdCurriculumManager.getInstance();
+        PhdCurriculum pCurriculum = new PhdCurriculum();
+        String oldCurriculum = pCurriculum.getDescription();
+        pCurriculum.setDescription("UpdateDescrizione");
+        pCurriculum.setName("UpdateInfbkbdsjhfdsigfhsuishyfiusgfgdsjhfgjdshgfvjhcxbjhvfgdsjhfgcxjhgvfjhxcgjfhxgdsjhfgxcjhgfjhx<gcfhjxzgjhcgxhj<cg<xjhgdcjhz<gcjhxzgcjhgxjhvcgxjhgvcjhsdgjfhgsdjhgvcjhxgvjhcxgjhvgxcjhvgjhxgzvjhcxgzvjhgjxhfgjhdsgfjhgdsjhfgsjhdagfjhagjfsagfjhsaormatica");
+        pCurriculum.setFK_Professor("ertghyuijhtredfg");
+        
+        instance.update(oldCurriculum, pCurriculum);
+        assertEquals(oldCurriculum, pCurriculum);
+        tearDown();
+    }
+    
+    @Test
+    public void testFormatNameUpdate() throws Exception {
+        System.out.println("update");
+        PhdCurriculumManager instance = PhdCurriculumManager.getInstance();
+        PhdCurriculum pCurriculum = new PhdCurriculum();
+        String oldCurriculum = pCurriculum.getDescription();
+        pCurriculum.setDescription("UpdateDescrizione");
+        pCurriculum.setName("&&&&&");
+        pCurriculum.setFK_Professor("ertghyuijhtredfg");
+        
+        instance.update(oldCurriculum, pCurriculum);
+        assertEquals(oldCurriculum, pCurriculum);
+        tearDown();
+    }
+    
+
+    /**
+     * Test of delete method, of class PhdCycleManager.
+     */
+    @Test
+    public void testDelete() throws Exception {
+        System.out.println("delete");
+        String idPhdCurriculumName = "Informatica";
+        PhdCurriculumManager instance =  PhdCurriculumManager.getInstance().getInstance();
+        instance.delete(idPhdCurriculumName);
+        
+        assertNull(instance);
+        tearDown();
+    }
+//    
+//
+//    /**
+//     * Test of getPhdCurriculumNameByCycle method, of class PhdCurriculumManager.
+//     */
+//    @Test
+//    public void testGetPhdCurriculumNameByCycle() throws Exception {
+//        System.out.println("getPhdCurriculumNameByCycle");
+//        int idPhdCycle = 0;
+//        PhdCurriculumManager instance = null;
+//        ArrayList<String> expResult = null;
+//        ArrayList<String> result = instance.getPhdCurriculumNameByCycle(idPhdCycle);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getPhdCurriculumNames method, of class PhdCurriculumManager.
+//     */
+//    @Test
+//    public void testGetPhdCurriculumNames() throws Exception {
+//        System.out.println("getPhdCurriculumNames");
+//        PhdCurriculumManager instance = null;
+//        ArrayList<String> expResult = null;
+//        ArrayList<String> result = instance.getPhdCurriculumNames();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getPhdCurriculumById method, of class PhdCurriculumManager.
+//     */
+//    @Test
+//    public void testGetPhdCurriculumById() throws Exception {
+//        System.out.println("getPhdCurriculumById");
+//        String phdCurriculumName = "";
+//        PhdCurriculumManager instance = null;
+//        PhdCurriculum expResult = null;
+//        PhdCurriculum result = instance.getPhdCurriculumById(phdCurriculumName);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//    
+}
---
+++ test/it/unisa/dottorato/phdCycle/PhdCycleManagerTest.java
@@ -0,0 +1,393 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.dottorato.phdCycle;
+
+import it.unisa.dottorato.exception.ConnectionException;
+import it.unisa.dottorato.exception.EntityNotFoundException;
+import it.unisa.dottorato.phdCurriculum.PhdCurriculumManager;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Fudo
+ */
+public class PhdCycleManagerTest {
+    
+    public PhdCycleManagerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+        
+        try {
+            PhdCycleManager.getInstance().delete("11");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(PhdCycleManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(PhdCycleManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(PhdCycleManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (EntityNotFoundException ex) {
+            Logger.getLogger(PhdCycleManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ConnectionException ex) {
+            Logger.getLogger(PhdCycleManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Test of getInstance method, of class PhdCycleManager.
+     */
+    @Test
+    public void testGetInstance() {
+        System.out.println("getInstance");
+        PhdCycleManager result = PhdCycleManager.getInstance();
+        assertNotNull(result);
+    }
+
+    /**
+     * Test of insert method, of class PhdCycleManager.
+     */
+    @Test
+    public void testInsert() throws Exception {
+        System.out.println("insert");
+        PhdCycle pCycle = new PhdCycle();
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        pCycle.setDescription("descrizione");
+        pCycle.setIdPhdCycle(11);
+        pCycle.setYear(2014);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        instance.insert(pCycle);
+        
+        PhdCycle result = instance.getPhdCycleById(11);
+        assertNotNull(result);
+        
+        tearDown();
+    }
+    
+    
+    @Test
+    public void testInsertEmptyIdPhdCycle() throws Exception {
+        System.out.println("insert");
+        PhdCycle pCycle = new PhdCycle();
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        pCycle.setDescription("descrizione");
+        pCycle.setIdPhdCycle(0);
+        pCycle.setYear(2014);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        instance.insert(pCycle);
+        
+        PhdCycle result = instance.getPhdCycleById(11);
+        assertNull(result);
+        
+        tearDown();
+    }
+    
+    
+    @Test
+    public void testInsertMaxValueIdPhdCycle() throws Exception {
+        System.out.println("insert");
+        PhdCycle pCycle = new PhdCycle();
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        pCycle.setDescription("descrizione");
+        pCycle.setIdPhdCycle(111);
+        pCycle.setYear(2014);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        instance.insert(pCycle);
+        
+        PhdCycle result = instance.getPhdCycleById(11);
+        assertNull(result);
+        
+        tearDown();
+    }
+    
+     @Test
+    public void testInsertStringIdPhdCycle() throws Exception {
+        System.out.println("insert");
+        PhdCycle pCycle = new PhdCycle();
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        pCycle.setDescription("descrizione");
+        pCycle.setIdPhdCycle("tette");
+        pCycle.setYear(2014);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        instance.insert(pCycle);
+        
+        PhdCycle result = instance.getPhdCycleById(11);
+        assertNull(result);
+        
+        tearDown();
+    }
+    
+     @Test
+    public void testInsertAlreadyExistIdPhdCycle() throws Exception {
+        System.out.println("insert");
+        PhdCycle pCycle = new PhdCycle();
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        pCycle.setDescription("descrizione");
+        pCycle.setIdPhdCycle(11);
+        pCycle.setYear(2014);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        instance.insert(pCycle);
+        instance.insert(pCycle);
+        
+        PhdCycle result = instance.getPhdCycleById(11);
+        assertNull(result);
+        
+        tearDown();
+    }
+    
+    @Test
+    public void testInsertMinimunYear() throws Exception {
+        System.out.println("insert");
+        PhdCycle pCycle = new PhdCycle();
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        pCycle.setDescription("descrizione");
+        pCycle.setIdPhdCycle(11);
+        pCycle.setYear(201);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        instance.insert(pCycle);
+        
+        PhdCycle result = instance.getPhdCycleById(11);
+        assertNull(result);
+        
+        tearDown();
+    }
+    
+    
+    @Test
+    public void testInsertMaxYear() throws Exception {
+        System.out.println("insert");
+        PhdCycle pCycle = new PhdCycle();
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        pCycle.setDescription("descrizione");
+        pCycle.setIdPhdCycle(11);
+        pCycle.setYear(20141);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        instance.insert(pCycle);
+        
+        PhdCycle result = instance.getPhdCycleById(11);
+        assertNull(result);
+        
+        tearDown();
+    }
+    
+       
+    @Test
+    public void testInsertNullFK_Professor() throws Exception {
+        System.out.println("insert");
+        PhdCycle pCycle = new PhdCycle();
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        pCycle.setDescription("descrizione");
+        pCycle.setIdPhdCycle(11);
+        pCycle.setYear(2014);
+        instance.insert(pCycle);
+        
+        PhdCycle result = instance.getPhdCycleById(11);
+        assertNull(result);
+        tearDown();
+    }
+
+    /**
+     * Test of update method, of class PhdCycleManager.
+     */
+    @Test
+    public void testUpdate() throws Exception {
+        System.out.println("update");
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        PhdCycle pCycle = new PhdCycle();
+        String oldIdPhdCycle = pCycle.getDescription();
+        pCycle.setDescription("UpdateDescrizione");
+        pCycle.setYear(2016);
+        pCycle.setIdPhdCycle(18);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        
+        instance.update(oldIdPhdCycle, pCycle);
+        assertEquals(oldIdPhdCycle, pCycle);
+        tearDown();
+    }
+    
+    @Test
+    public void testEmptyIDUpdate() throws Exception {
+        System.out.println("update");
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        PhdCycle pCycle = new PhdCycle();
+        String oldIdPhdCycle = pCycle.getDescription();
+        pCycle.setDescription("UpdateDescrizione");
+        pCycle.setYear(2016);
+        pCycle.setIdPhdCycle(0);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        
+        instance.update(oldIdPhdCycle, pCycle);
+        assertEquals(oldIdPhdCycle, pCycle);
+        tearDown();
+    }
+    @Test
+    public void testMaxIDUpdate() throws Exception {
+        System.out.println("update");
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        PhdCycle pCycle = new PhdCycle();
+        String oldIdPhdCycle = pCycle.getDescription();
+        pCycle.setDescription("UpdateDescrizione");
+        pCycle.setYear(2016);
+        pCycle.setIdPhdCycle(188);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        
+        instance.update(oldIdPhdCycle, pCycle);
+        assertEquals(oldIdPhdCycle, pCycle);
+        tearDown();
+    }
+    
+    @Test
+    public void testFormatIDUpdate() throws Exception {
+        System.out.println("update");
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        PhdCycle pCycle = new PhdCycle();
+        String oldIdPhdCycle = pCycle.getDescription();
+        pCycle.setDescription("UpdateDescrizione");
+        pCycle.setYear(2016);
+        pCycle.setIdPhdCycle("SS");
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        
+        instance.update(oldIdPhdCycle, pCycle);
+        assertEquals(oldIdPhdCycle, pCycle);
+        tearDown();
+    }
+    
+    @Test
+    public void testExistUpdate() throws Exception {
+        System.out.println("update");
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        PhdCycle pCycle = new PhdCycle();
+        String oldIdPhdCycle = pCycle.getDescription();
+        pCycle.setDescription("UpdateDescrizione");
+        pCycle.setYear(2016);
+        pCycle.setIdPhdCycle(18);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        
+        instance.update(oldIdPhdCycle, pCycle);
+        instance.update(oldIdPhdCycle, pCycle);
+        assertEquals(oldIdPhdCycle, pCycle);
+        tearDown();
+    }
+    
+    @Test
+    public void testMinYearUpdate() throws Exception {
+        System.out.println("update");
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        PhdCycle pCycle = new PhdCycle();
+        String oldIdPhdCycle = pCycle.getDescription();
+        pCycle.setDescription("UpdateDescrizione");
+        pCycle.setYear(201);
+        pCycle.setIdPhdCycle(18);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        
+        instance.update(oldIdPhdCycle, pCycle);
+        assertEquals(oldIdPhdCycle, pCycle);
+        tearDown();
+    }
+    
+    @Test
+    public void testMaxYearUpdate() throws Exception {
+        System.out.println("update");
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        PhdCycle pCycle = new PhdCycle();
+        String oldIdPhdCycle = pCycle.getDescription();
+        pCycle.setDescription("UpdateDescrizione");
+        pCycle.setYear(201111);
+        pCycle.setIdPhdCycle(18);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        
+        instance.update(oldIdPhdCycle, pCycle);
+        assertEquals(oldIdPhdCycle, pCycle);
+        tearDown();
+    }
+    
+    
+    @Test
+    public void testFormatYearUpdate() throws Exception {
+        System.out.println("update");
+        PhdCycleManager instance = PhdCycleManager.getInstance();
+        PhdCycle pCycle = new PhdCycle();
+        String oldIdPhdCycle = pCycle.getDescription();
+        pCycle.setDescription("UpdateDescrizione");
+        pCycle.setYear("year");
+        pCycle.setIdPhdCycle(18);
+        pCycle.setFK_Professor("ertghyuijhtredfg");
+        
+        instance.update(oldIdPhdCycle, pCycle);
+        assertEquals(oldIdPhdCycle, pCycle);
+        tearDown();
+    }
+    
+
+    /**
+     * Test of delete method, of class PhdCycleManager.
+     */
+    @Test
+    public void testDelete() throws Exception {
+        System.out.println("delete");
+        String idPhdCycle = "15";
+        PhdCycleManager instance =  PhdCycleManager.getInstance();
+        instance.delete(idPhdCycle);
+        
+        assertNull(instance);
+        tearDown();
+    }
+    
+    
+    
+//
+//    /**
+//     * Test of getPhdCycleById method, of class PhdCycleManager.
+//     */
+//    @Test
+//    public void testGetPhdCycleById() throws Exception {
+//        System.out.println("getPhdCycleById");
+//        int idPhdCycle = 0;
+//        PhdCycleManager instance = null;
+//        PhdCycle expResult = null;
+//        PhdCycle result = instance.getPhdCycleById(idPhdCycle);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getPhdCyclesIds method, of class PhdCycleManager.
+//     */
+//    @Test
+//    public void testGetPhdCyclesIds() throws Exception {
+//        System.out.println("getPhdCyclesIds");
+//        PhdCycleManager instance = null;
+//        ArrayList<String> expResult = null;
+//        ArrayList<String> result = instance.getPhdCyclesIds();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+}
